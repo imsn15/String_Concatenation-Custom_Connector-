cc96cdf912a361a3d8cc4e151d9ed62688c07b74
Sainath Bandari <bandarisainath15@gmail.com>
2023-05-16 16:36:32 +0530

    Create ConcatStringConnection.java

---
+++ src/main/java/com/eaiesb/internal/ConcatStringConnection.java
@@ -0,0 +1,22 @@
+package com.eaiesb.internal;
+
+
+/**
+ * This class represents an extension connection just as example (there is no real connection with anything here c:).
+ */
+public final class ConcatStringConnection {
+
+  private final String id;
+
+  public ConcatStringConnection(String id) {
+    this.id = id;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void invalidate() {
+    // do something to invalidate this connection!
+  }
+}
